e92ee392b82dfb601c159b3c97d1bb71d96b179e
MariamKamel <mariammkamel98@gmail.com>
2020-05-16 06:45:59 +0200

    edit profile bugs fixed

--- mobile/src/screens/Profile/index.jsx
+++ mobile/src/screens/Profile/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { View, TextInput, Text, Image, AsyncStorage } from "react-native";
+import { View, Text, Keyboard, AsyncStorage } from "react-native";
 import { Input, Icon } from "@ui-kitten/components";
 import styles from "./styles";
 import { TouchableOpacity } from "react-native-gesture-handler";
@@ -10,10 +10,6 @@ import { createStackNavigator } from "@react-navigation/stack";
 
 function Profile({ navigation, setToken }) {
   const [userInfo, setUserInfo] = useState({});
-  const [nameFocused, setNameFocused] = useState(false);
-  const [emailFocused, setEmailFocused] = useState(false);
-  const [budgetFocused, setBudgetFocused] = useState(false);
-  const [passwordFocused, setPasswordFocused] = useState(false);
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState(undefined);
@@ -26,7 +22,6 @@ function Profile({ navigation, setToken }) {
       monthlyBudget,
       password
     });
-    console.log(res.status);
   };
 
   const handleLogOut = async () => {
@@ -34,14 +29,11 @@ function Profile({ navigation, setToken }) {
     AsyncStorage.removeItem("userToken");
   };
 
-  useEffect(() => {
-    console.log("effect", name, monthlyBudget);
-  }, [name, monthlyBudget]);
+  useEffect(() => {}, [name, monthlyBudget]);
 
   useEffect(() => {
     setName(userInfo.name);
     setMonthlyBudget(userInfo.monthlyBudget);
-    console.log(monthlyBudget, name);
   }, [userInfo]);
 
   useEffect(() => {
@@ -50,7 +42,6 @@ function Profile({ navigation, setToken }) {
       if (res.status === 200) {
         setUserInfo(res.data);
 
-        console.log(userInfo);
         return;
       }
       return undefined;
@@ -61,7 +52,6 @@ function Profile({ navigation, setToken }) {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <Text style={styles.text}>Profile</Text>
         <Avatar size={80} source={download} />
       </View>
       <Input
@@ -69,14 +59,10 @@ function Profile({ navigation, setToken }) {
         value={userInfo.email}
         disabled={true}
         style={styles.input}
-        onFocus={() => setEmailFocused(true)}
-        onBlur={() => setEmailFocused(false)}
       />
       <Input
         placeholder="Name"
         style={styles.input}
-        onFocus={() => setNameFocused(true)}
-        onBlur={() => setNameFocused(false)}
         onChangeText={value => setName(value)}
         defaultValue={userInfo.name}
       />
@@ -84,25 +70,24 @@ function Profile({ navigation, setToken }) {
         placeholder="Password"
         secureTextEntry
         style={styles.input}
-        onFocus={() => setPasswordFocused(true)}
-        onBlur={() => setPasswordFocused(false)}
         onChangeText={value => setPassword(value)}
       />
       <Input
         placeholder="Budget"
         style={styles.input}
-        onFocus={() => {
-          setBudgetFocused(true);
-          console.log(userInfo.monthlyBudget);
-        }}
-        onBlur={() => setBudgetFocused(false)}
         onChangeText={value => setMonthlyBudget(parseInt(value))}
         defaultValue={
           userInfo.monthlyBudget && userInfo.monthlyBudget.toString()
         }
       />
 
-      <TouchableOpacity style={styles.saveButton} onPress={() => handleEdit()}>
+      <TouchableOpacity
+        style={styles.saveButton}
+        onPress={() => {
+          handleEdit();
+          Keyboard.dismiss();
+        }}
+      >
         <Text
           style={{
             fontFamily: "OpenSans-Bold",
